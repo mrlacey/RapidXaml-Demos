d72ae342d9158994b5288136c882af7a8cf96032
Matt Lacey <matt@mrlacey.co.uk>
2020-03-20 00:50:40 +0000

    Add MyUserControlAnalyzer for demos & testing

---
+++ CustomAnalyzer/MyUserControlAnalyzer.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RapidXaml;
+
+namespace ExampleDemoAnalyzer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DoesReportWhenShould()
+        {
+            var analyzer = new MyUserControlAnalyzer();
+
+            var testElement = CustomAnalysisTestHelper.StringToElement("<MyUserControl />");
+
+            var analysisResult = analyzer.Analyze(testElement);
+
+            Assert.AreEqual(1, analysisResult.Actions.Count);
+            Assert.AreEqual(ActionType.AddAttribute, analysisResult.Actions[0].Action);
+            Assert.AreEqual("Name", analysisResult.Actions[0].Name);
+        }
+
+        [TestMethod]
+        public void DoesNotReportWhenShouldNot()
+        {
+            var analyzer = new MyUserControlAnalyzer();
+
+            var testElement = CustomAnalysisTestHelper.StringToElement("<MyUserControl Name=\"something\" />");
+
+            var analysisResult = analyzer.Analyze(testElement);
+
+            Assert.IsTrue(analysisResult.IsNone);
+        }
+    }
+}
---
+++ CustomAnalyzer/MyUserControlAnalyzer/MyUserControlAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RapidXaml;
+
+namespace ExampleDemoAnalyzer
+{
+    public class MyUserControlAnalyzer : ICustomAnalyzer
+    {
+        public string TargetType() => "MyUserControl";
+
+        public AnalysisActions Analyze(RapidXamlElement element)
+        {
+            if (element.ContainsAttribute("Name"))
+            {
+                return AnalysisActions.None;
+            }
+            else
+            {
+                return AnalysisActions.AddAttribute(
+                    errorType: RapidXamlErrorType.Warning,
+                    code: "RXD001",
+                    description: "Always set the 'Name' property on 'MyUserControl'.",
+                    actionText: "Add 'Name' attribute",
+                    addAttributeName: "Name",
+                    addAttributeValue: "MyControl",
+                    moreInfoUrl: "https://github.com/mrlacey/RapidXaml-Demos/blob/master/rxd001.md"
+                    );
+            }
+        }
+    }
+}
