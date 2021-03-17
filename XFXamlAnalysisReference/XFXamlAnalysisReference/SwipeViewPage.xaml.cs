0b4ad8274c6f7eda13b23bd4de2002cb30ddff16
Matt Lacey <matt@mrlacey.co.uk>
2021-03-17 15:05:01 +0000

    Add initial Xamarin.Forms reference

---
+++ XFXamlAnalysisReference/XFXamlAnalysisReference/SwipeViewPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsXamlAnalysisReference
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SwipeViewPage : ContentPage
+    {
+        public SwipeViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
