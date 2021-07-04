f5b1464470ca0e0c76735d94d2b226465186629f
Matt Lacey <matt@mrlacey.co.uk>
2021-07-04 21:37:39 +0100

    Create MainViewModel.cs

---
+++ UwpXamlAnalysisReference/UwpXamlAnalysisReference/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwpXamlAnalysisReference.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetProperty<T>(T value, string propertyName)
+        {
+        }
+    }
+}
